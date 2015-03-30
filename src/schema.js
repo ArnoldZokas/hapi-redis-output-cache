977410386a0156f2bf015e78991647ccfa6cf6e7
Arnold Zokas <arnold.zokas@coderoom.net>
2015-03-30 20:54:26 +0100

    updated schema (#2)

--- src/schema.js
+++ src/schema.js
@@ -6,7 +6,8 @@ module.exports = {
     host: joi.string().required(),
     port: joi.number().min(1),
     varyByHeaders: joi.array(),
-    ttl: joi.number().min(1).required(),
+    staleIn: joi.number().min(1).required(),
+    expiresIn: joi.number().min(1).required(),
     onCacheMiss: joi.func(),
     onError: joi.func()
 };
--- test/plugin option validation.js
+++ test/plugin option validation.js
@@ -6,7 +6,7 @@ var expect = require('expect.js'),
 describe('plugin option validation', function() {
     describe('given null host', function() {
         it('should return error', function () {
-            plugin.register(null, { ttl: 1 }, function(err) {
+            plugin.register(null, { staleIn: 1, expiresIn: 1 }, function(err) {
                 expect(err.toString()).to.equal('ValidationError: child "host" fails because ["host" is required]');
             });
         });
@@ -14,7 +14,7 @@ describe('plugin option validation', function() {
 
     describe('given empty host', function() {
         it('should return error', function () {
-            plugin.register(null, { host: '', ttl: 1 }, function(err) {
+            plugin.register(null, { host: '', staleIn: 1, expiresIn: 1 }, function(err) {
                 expect(err.toString()).to.equal('ValidationError: child "host" fails because ["host" is not allowed to be empty]');
             });
         });
@@ -22,32 +22,56 @@ describe('plugin option validation', function() {
 
     describe('given non-array varyByHeaders', function() {
         it('should return error', function () {
-            plugin.register(null, { host: '127.0.0.1', varyByHeaders: 'string', ttl: 1 }, function(err) {
+            plugin.register(null, { host: '127.0.0.1', varyByHeaders: 'string', staleIn: 1, expiresIn: 1 }, function(err) {
                 expect(err.toString()).to.equal('ValidationError: child "varyByHeaders" fails because ["varyByHeaders" must be an array]');
             });
         });
     });
 
-    describe('given null ttl', function() {
+    describe('given null staleIn', function() {
         it('should return error', function () {
-            plugin.register(null, { host: '127.0.0.1' }, function(err) {
-                expect(err.toString()).to.equal('ValidationError: child "ttl" fails because ["ttl" is required]');
+            plugin.register(null, { host: '127.0.0.1', expiresIn: 1 }, function(err) {
+                expect(err.toString()).to.equal('ValidationError: child "staleIn" fails because ["staleIn" is required]');
             });
         });
     });
 
-    describe('given 0 ttl', function() {
+    describe('given 0 staleIn', function() {
         it('should return error', function () {
-            plugin.register(null, { host: '127.0.0.1', ttl: 0 }, function(err) {
-                expect(err.toString()).to.equal('ValidationError: child "ttl" fails because ["ttl" must be larger than or equal to 1]');
+            plugin.register(null, { host: '127.0.0.1', staleIn: 0, expiresIn: 1 }, function(err) {
+                expect(err.toString()).to.equal('ValidationError: child "staleIn" fails because ["staleIn" must be larger than or equal to 1]');
+            });
+        });
+    });
+
+    describe('given null expiresIn', function() {
+        it('should return error', function () {
+            plugin.register(null, { host: '127.0.0.1', staleIn: 1 }, function(err) {
+                expect(err.toString()).to.equal('ValidationError: child "expiresIn" fails because ["expiresIn" is required]');
+            });
+        });
+    });
+
+    describe('given 0 expiresIn', function() {
+        it('should return error', function () {
+            plugin.register(null, { host: '127.0.0.1', staleIn: 1, expiresIn: 0 }, function(err) {
+                expect(err.toString()).to.equal('ValidationError: child "expiresIn" fails because ["expiresIn" must be larger than or equal to 1]');
             });
         });
     });
 
     describe('given invalid onCacheMiss handler', function() {
         it('should return error', function () {
-            plugin.register(null, { host: '127.0.0.1', ttl: 1, onCacheMiss: '' }, function(err) {
+            plugin.register(null, { host: '127.0.0.1', staleIn: 1, expiresIn: 1, onCacheMiss: '' }, function(err) {
                 expect(err.toString()).to.equal('ValidationError: child "onCacheMiss" fails because ["onCacheMiss" must be a Function]');
+            });
+        });
+    });
+
+    describe('given invalid onError handler', function() {
+        it('should return error', function () {
+            plugin.register(null, { host: '127.0.0.1', staleIn: 1, expiresIn: 1, onError: '' }, function(err) {
+                expect(err.toString()).to.equal('ValidationError: child "onError" fails because ["onError" must be a Function]');
             });
         });
     });
