fbe5ead7c34ce2d82002d52c41e62ec883e2fc09
Arnold Zokas <arnold.zokas@coderoom.net>
2015-03-30 10:20:51 +0100

    refactored grunt tasks

--- grunt_tasks/jshint.js
+++
@@ -1,9 +0,0 @@
-var task = {
-    options: {
-        jshintrc: '.jshintrc',
-        reporter: require('jshint-stylish')
-    },
-    all: ['*.js', 'src/**/*.js', 'test/**/*.js']
-};
-
-module.exports = task;
