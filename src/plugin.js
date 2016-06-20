ad8102b029f927259955adbff08cf81905849924
Arnold Zokas <arnold.zokas@coderoom.net>
2016-06-20 10:58:54 +0100

    housekeeping

--- src/plugin.js
+++ src/plugin.js
@@ -2,16 +2,6 @@
 
 const joi                = require('joi');
 const cacheKeyGenerator  = require('./cacheKeyGenerator');
-//var hoek = require('hoek');
-
-// var isisCacheable = function(req) {
-//   // TODO: ensure non-get requests are not cached
-//     if(req.route.method !== 'get') {
-//         return false;
-//     }
-//
-//     return req.route.settings.plugins['hapi-redis-output-cache'].isCacheable;
-// };
 
 exports.register = function (plugin, options, next) {
     const validation = joi.validate(options, require('./schema'));
@@ -20,14 +10,6 @@ exports.register = function (plugin, options, next) {
         return next(validation.error);
     }
 
-    // var defaultCacheOptions = {
-    //   varyByHeaders: options.varyByHeaders,
-    //   staleIn: options.staleIn,
-    //   expiresIn: options.expiresIn,
-    //   partition: options.partition
-    // };
-    //
-
     const client = require('redis').createClient({
         host: options.host,
         port: options.port || 6379
@@ -35,53 +17,11 @@ exports.register = function (plugin, options, next) {
 
     // // TODO: test error behaviour
     // client.on('error', options.onError || function() {});
-    //
+
     plugin.ext('onPreHandler', (req, reply) => {
-        // req.outputCache = req.outputCache || {
-        //     isStale: null,
-        //     data: null
-        // };
-        //
-        // if(isisCacheable(req) === false) {
-        //     return reply.continue();
-        // }
-        //
         // if(client.connected === false) {
         //     return reply.continue();
         // }
-        //
-        // // TODO: test to ensure route-specific options are respected
-        // var routeSettings = hoek.applyToDefaults(defaultCacheOptions, req.route.settings.plugins['hapi-redis-output-cache']);
-        // var cacheKey = generateCacheKey(req, routeSettings);
-        //
-        // client.get(cacheKey, function(err, data) {
-        //     if(err) {
-        //         return reply.continue();
-        //     }
-        //
-        //     if(data) {
-        //         var cachedValue = JSON.parse(data);
-        //         var currentTime = Math.floor(new Date() / 1000);
-        //         req.outputCache.data = cachedValue;
-        //
-        //         if(cachedValue.expiresOn > currentTime) {
-        //             req.outputCache.isStale = false;
-        //
-        //             var response  = reply(cachedValue.payload);
-        //             response.code(cachedValue.statusCode);
-        //
-        //             var keys = Object.keys(cachedValue.headers);
-        //             for(var i = 0; i < keys.length; i++) {
-        //                 var key = keys[i];
-        //                 response.header(key, cachedValue.headers[key]);
-        //             }
-        //
-        //             return;
-        //         }
-        //     }
-        //
-        //     return reply.continue();
-        // });
 
         const routeOptions = req.route.settings.plugins['hapi-redis-output-cache'] || {};
         if(routeOptions.isCacheable !== true) {
@@ -133,10 +73,6 @@ exports.register = function (plugin, options, next) {
         // if(client.connected === false) {
         //     return reply.continue();
         // }
-        //
-        // if(req.outputCache && req.outputCache.isStale && req.response.statusCode) {
-        //     client.setex(cacheKey, routeSettings.expiresIn, JSON.stringify(cacheValue));
-        // }
 
         const routeOptions = req.route.settings.plugins['hapi-redis-output-cache'] || {};
         if(routeOptions.isCacheable !== true) {
@@ -169,8 +105,6 @@ exports.register = function (plugin, options, next) {
             return reply.continue();
         }
 
-        options.onCacheMiss(req, reply);
-
         const cacheKey = cacheKeyGenerator.generateCacheKey(req, options);
 
         const cacheValue = {
@@ -181,6 +115,8 @@ exports.register = function (plugin, options, next) {
         };
 
         client.setex(cacheKey, options.expiresIn, JSON.stringify(cacheValue));
+
+        options.onCacheMiss(req, reply);
 
         return reply.continue();
     });
